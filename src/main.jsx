7f37e14c85c2f088ecd45caab6f00c9ce6057459
shahadot99999 <shahadot99999@gmail.com>
2025-07-29 22:47:50 +0600

    router set up

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/router.jsx'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />
+     {/* <RouterProvider router={router} /> */}
+     <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
---
+++ src/routes/router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Home Layout</h1>,
+    },
+    {
+        path: "/booking",
+        element: <h1>Booking Layout</h1>,
+    },
+    {
+        path: "auth",
+        element: <h1>Login Layout</h1>,
+    },
+    {
+        path: "*",
+        element: <h1>Error</h1>,
+    },
+])
+
+export default router;
