dc87f1bea27487c324f57b269c8d92f042f83037
shahadot99999 <shahadot99999@gmail.com>
2025-07-30 19:37:18 +0600

    navbar is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,43 @@
+// import logo from "../assets/logo.png";
+import img from '../assets/logo.png';
 
 
 const Navbar = () => {
     return (
-        <div className="flex justify-between">
-          <div className=""></div>
-          <div className="searchbar"></div>
-          <div className=""></div>
-          <div className="login"></div>
+         <div className="flex justify-between items-center p-4 bg-white shadow-sm">
+            {/* Logo */}
+            
+                <div className="flex items-center">
+                <img 
+                    src={img} 
+                    alt="Travel Guru Logo" 
+                    className="h-10" // Adjust height as needed
+                />
+            </div>
+            
+            {/* Search Bar */}
+            <div className="flex-1 mx-8">
+                <input 
+                    type="text" 
+                    placeholder="Search your Destination..." 
+                    className="w-full py-2 px-4 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+            </div>
+            
+            {/* Navigation Links */}
+            <div className="flex space-x-6">
+                <a href="#" className="text-gray-700 hover:text-blue-600">News</a>
+                <a href="#" className="text-gray-700 hover:text-blue-600">Destination</a>
+                <a href="#" className="text-gray-700 hover:text-blue-600">Blog</a>
+                <a href="#" className="text-gray-700 hover:text-blue-600">Contact</a>
+            </div>
+            
+            {/* Login Button */}
+            <div className="ml-6">
+                <button className="bg-blue-600 text-white py-2 px-6 rounded-full hover:bg-blue-700 transition duration-300">
+                    Login
+                </button>
+            </div>
         </div>
     );
 };
