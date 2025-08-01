7904d325d365137d5015db07b9d618fa9678b90b
shahadot99999 <shahadot99999@gmail.com>
2025-08-01 07:55:20 +0600

    Main section are processing

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,21 +1,23 @@
 import Navbar from "../components/Navbar";
 
+import Mainsections from "../components/Mainsections";
+
 
 const HomeLayout = () => {
     return (
-        <div className="font-poppins">
-          <nav className="w-11/12 mx-auto py-2">
-           <Navbar></Navbar>
-          </nav>
-          
-          <main className="w-11/12 mx-auto pt-4">
-             <div>
-              <div className="left col-4">left</div>
-              <div className="right col-8">Right</div>
-             </div>
-          </main>
-          <footer></footer>
-        </div>
+      <div className="font-poppins">
+        <nav className="w-11/12 mx-auto py-2">
+          <Navbar></Navbar>
+        </nav>
+
+        <main className="w-11/12 mx-auto pt-8">
+          <Mainsections></Mainsections>
+        </main>
+
+
+
+        <footer></footer>
+      </div>
     );
 };
 
